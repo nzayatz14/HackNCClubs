df3c65294ce2c85a2b82efbccba7d198f69e8410
nzayatz14 <nzayatz14@hotmail.com>
2015-10-10 11:08:26 -0400

    pods

---
+++ HackNCClubs/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  HackNCClubs
+//
+//  Created by Nick Zayatz on 10/10/15.
+//  Copyright © 2015 Nick Zayatz. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        print("wassup guys")
+    }
+    
+}
